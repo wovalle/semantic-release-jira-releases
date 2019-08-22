a7bd2ad544115210d49703bd998b01435b181778
willy.ovalle@klarna.com <willy.ovalle@klarna.com>
2019-08-22 16:48:58 +0200

    fix: wrong path in tests

--- test/test.ts
+++ test/test.ts
@@ -1,4 +1,4 @@
-import { getTickets } from "../lib/success";
+import { getTickets } from "../lib/publish";
 import { pluginConfig, context } from "./fakedata";
 import { PluginConfig } from "../lib/types";
 
